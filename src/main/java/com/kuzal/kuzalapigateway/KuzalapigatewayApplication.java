f3e54d149cbf11c9430a4dc0f58f93ef2cb9cdfb
고태광 <kotaekwang@gotaegwang-ui-MacBook-Pro.local>
2019-05-04 16:06:04 +0900

    init kuzal apiGW project

---
+++ src/main/java/com/kuzal/kuzalapigateway/KuzalapigatewayApplication.java
@@ -0,0 +1,15 @@
+package com.kuzal.kuzalapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class KuzalapigatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuzalapigatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kuzal/kuzalapigateway/KuzalapigatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kuzal.kuzalapigateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KuzalapigatewayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
